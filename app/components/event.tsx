3aa1685308d04a879c6842bfc62cb450ff826980
Muskan Fatima <160476952+muskan-fatim@users.noreply.github.com>
2025-02-23 18:10:10 +0500

    Update
    Add id for navigation

--- app/components/event.tsx
+++ app/components/event.tsx
@@ -18,7 +18,7 @@ const EventGrid: React.FC<EventGridProps> = ({ events }) => {
     }
 
     return (
-        <div className="p-6">
+        <div className="p-6" id="event">
             <h1 className="text-3xl font-bold text-center mb-6">Upcoming Tech Events</h1>
             
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
