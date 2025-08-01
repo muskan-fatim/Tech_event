b8400137d86673badcdbc90562c4cbb82182e4cb
kris2005mehra <krismehra22@gmail.com>
2025-08-01 17:43:20 +0530

    commit

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState } from 'react';
 
-export default function AuthForm() {
+export default function authForm() {
   const [isLogin, setIsLogin] = useState(true);
 
   return (
