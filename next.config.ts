aff194a3bc4b5e66d8e5344b3083a7feb5802942
kris2005mehra <krismehra22@gmail.com>
2025-08-01 17:49:45 +0530

    commmit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-eslint:{
-  ignoreDuringBuilds: true
-}
+  experimental: {
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
